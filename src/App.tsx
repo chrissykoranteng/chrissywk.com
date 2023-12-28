69939e64b9e3777dc97571451b41206879c8114c
omesb <omesgit@gmail.com>
2023-12-28 12:57:18 +0100

    init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>This is Chrissy's Website</h1>
+      <p>Glad you're here.</p>
+      <p>Do you wanna know more?</p>
+      <button>Discover your potential</button>
+    </>
+  );
+}
+
+export default App;
